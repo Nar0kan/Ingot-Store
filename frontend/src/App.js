aad2723996c59490ccbba3254ffd3b2593dd728a
Narokan <nick.kirichenko.dev@gmail.com>
2023-07-15 14:40:30 +0300

    static frontend update
    Urls, product pages, frontend styling with bootstrap add

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import Header from './components/header'
+import Footer from './components/footer'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+function App() {
+  return (
+    <Router>
+      <Header />
+        <main className="py-3">
+          <Container>
+              <Routes>
+                <Route path='/' element={ <HomeScreen /> } exact />
+                <Route path='/product/:id' element={ <ProductScreen /> } />
+              </Routes>
+          </Container>
+        </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
